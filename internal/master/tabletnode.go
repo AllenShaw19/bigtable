1be945c338fdb89434b97fe7d5b19bb9b23267c4
allen <allenshaw19@gmail.com>
2024-02-25 22:09:23 +0800

    feat(master):update tabletnode manager

--- internal/master/tabletnode.go
+++ internal/master/tabletnode.go
@@ -5,8 +5,9 @@ import "sync"
 type NodeState int
 
 type TabletNode struct {
-	mutex sync.Mutex
-	addr  string
-	uuid  string
-	state NodeState
+	mutex     sync.Mutex
+	addr      string
+	uuid      string
+	state     NodeState
+	timestamp int64
 }
--- internal/master/tabletnode_manager.go
+++ internal/master/tabletnode_manager.go
@@ -1,4 +1,12 @@
 package master
 
+import "sync"
+
+type TabletNodeList map[string]*TabletNode
+
 type TabletNodeManager struct {
+	mutex              sync.Mutex
+	master             *Master
+	tabletNodeList     TabletNodeList
+	reconnectingTsList map[string]int64
 }
